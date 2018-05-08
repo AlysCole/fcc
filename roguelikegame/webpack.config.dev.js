5d8b0d47a8fa1e5a988665dea0d44a53e7e6f24c
Alyssa Alvarez <alyscole@outlook.com>
2018-05-08 12:27:38 +0800

    Modify config files
     - Separate run scripts in package.json to "dev" and "start".
     - Install prettier as a dev dependency.
     - Fix devServer config. webpack-dev-server did not load compiled
       files. Set devServer.publicPath to match output.publicPath.

--- roguelikegame/webpack.config.dev.js
+++ roguelikegame/webpack.config.dev.js
@@ -1,59 +1,56 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, 'src/main.jsx')
+    main: path.resolve(__dirname, "src/main.jsx")
   },
   output: {
-    filename: 'main.js',
-    path: path.join(__dirname, 'public'),
-    publicPath: '/public/'
+    filename: "main.js",
+    path: path.join(__dirname, "public"),
+    publicPath: "/public/"
   },
   devServer: {
-    publicPath: '/',
+    contentBase: "./",
+    publicPath: "/public/",
     inline: true
   },
-  devtool: 'source-map',
-  plugins: [
-    new ExtractTextPlugin('./styles.css')
-  ],
-  module: {
+  devtool: "source-map",
+  plugins: [new ExtractTextPlugin("./styles.css")],
+  module: 
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: [/node_modules/, /styles/, /public/],
-        include: path.join(__dirname, 'src'),
-        loader: 'babel-loader',
-        query:
-        {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-class-properties']
+        include: path.join(__dirname, "src"),
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react"],
+          plugins: ["transform-class-properties"]
         }
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: [/node-modules/, /public/],
-        include: path.join(__dirname, 'src'),
-        query:
-          {
-            presets: ['es2015', 'react']
-          }
+        include: path.join(__dirname, "src"),
+        query: {
+          presets: ["es2015", "react"]
+        }
       },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader!sass-loader'
+          fallback: "style-loader",
+          use: "css-loader!sass-loader"
         })
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
+          fallback: "style-loader",
+          use: "css-loader"
         })
       }
     ]
